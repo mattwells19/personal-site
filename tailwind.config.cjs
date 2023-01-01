af8cc88650484e7239b708649e39b24b50195d62
Matt Wells <mattwells878@gmail.com>
2023-01-01 18:26:38 -0500

    Maybe a personal site, resume added

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  // content: [
+  //   "./src/components/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  //   "./src/icons/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  //   "./src/layouts/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  //   "./src/pages/index.astro",
+  // ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
