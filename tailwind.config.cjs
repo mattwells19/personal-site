bc17edc5a166a8aaedc3602ddb005638e41e8f15
Matt Wells <mattwells878@gmail.com>
2023-01-01 20:57:38 -0500

    Add download to resume, favicons, and fix main page on mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  // content: [
-  //   "./src/components/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-  //   "./src/icons/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-  //   "./src/layouts/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-  //   "./src/pages/index.astro",
-  // ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        // used in resume.astro for the download btn
+        "bounce-twice": "bounce 1s 2.5",
+      },
+    },
   },
   plugins: [],
 };
